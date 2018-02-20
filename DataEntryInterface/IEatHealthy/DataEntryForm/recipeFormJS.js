5de6949c52da8b8788990caec06b30d919764845
chuy102 <chuy102jose@gmail.com>
2018-02-20 12:42:50 -0800

    Add data entry form

---
+++ DataEntryInterface/IEatHealthy/DataEntryForm/recipeFormJS.js
@@ -0,0 +1,38 @@
+//Javascript file for recipe form
+
+
+var stepCounter = 5; //Number of steps to display.
+
+document.onkeypress = stopSubmit; //Checks for any key press event.
+
+//Stops the default action of submitting a form when the enter key is pressed.
+function stopSubmit(event)
+{
+	if (event.keyCode == 13)
+	{
+		return false;
+	}
+}
+
+//Adds another input field.
+function addInput(divName, placeholder)
+{
+	var newDiv = document.createElement('div');
+	newDiv.innerHTML = "<input type='text' placeholder='"+ placeholder + "'name='" + divName + "'>";
+	document.getElementById(divName).appendChild(newDiv);
+}
+
+//Adds another input field (specifically adds a 'step' input field).
+function addStep(divName, placeHolder)
+{
+	var newdiv = document.createElement('div');
+	newdiv.innerHTML = "<textarea  placeholder='"+ placeHolder + " " + stepCounter + "'name='" + divName + "'></textarea>";
+	document.getElementById(divName).appendChild(newdiv);
+	stepCounter++;
+}
+
+function submitForm()
+{
+	
+}
+
